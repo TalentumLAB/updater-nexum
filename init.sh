f64e66e801f81ee58bd64b6fc44edbdf855be9a9
Jose Ramiro Ramirez Vargas <especialista.servidores@talentum.edu.co>
2023-03-25 23:06:55 -0500

    Add init.sh and program binary of jq

--- init.sh
+++ init.sh
@@ -2,6 +2,6 @@
 
 cp ./jq-linux64 /bin/jq
 
-echo "@reboot /bin/bash /git/updater-nexum/docker-compose-update.sh" >> /var/spool/cron/crontabs/ramiro
+echo "@reboot /bin/bash /git/updater-nexum/docker-compose-update.sh" >> /var/spool/cron/crontabs/talentum
 
 chmod +x ./*.sh
