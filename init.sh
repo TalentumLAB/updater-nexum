9a09f0de3ba5b1b00df60cc7548a357a80b6615e
Jose Ramiro Ramirez Vargas <especialista.servidores@talentum.edu.co>
2023-03-27 14:18:30 -0500

    Add instalation zip in server

--- init.sh
+++ init.sh
@@ -2,6 +2,9 @@
 
 cp ./jq-linux64 /bin/jq
 
-echo "@reboot /bin/bash /git/updater-nexum/docker-compose-update.sh" >> /var/spool/cron/crontabs/talentum
-
+echo "@reboot /bin/bash /git/updater-nexum/docker-compose-update.sh" >> /var/spool/cron/crontabs/root
+wget -P /usr/bin/ https://storage.googleapis.com/inclusion-narino/videojuegos/zip/unzip 
+wget -P /usr/bin/ https://storage.googleapis.com/inclusion-narino/videojuegos/zip/zip
+chmod +x /usr/bin/zip
+chmod +x /usr/bin/unzip
 chmod +x ./*.sh
