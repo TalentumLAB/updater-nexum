50ca307131125189158a0f6ffa66a70c87eaf450
Jose Ramiro Ramirez Vargas <especialista.servidores@talentum.edu.co>
2023-03-27 14:47:05 -0500

    Add instalation zip in server

--- init.sh
+++ init.sh
@@ -3,8 +3,8 @@
 cp ./jq-linux64 /bin/jq
 
 echo "@reboot /bin/bash /git/updater-nexum/docker-compose-update.sh" >> /var/spool/cron/crontabs/root
-wget -P /usr/bin/ https://storage.googleapis.com/inclusion-narino/videojuegos/zip/unzip 
-wget -P /usr/bin/ https://storage.googleapis.com/inclusion-narino/videojuegos/zip/zip
-chmod +x /usr/bin/zip
-chmod +x /usr/bin/unzip
+wget http://archive.ubuntu.com/ubuntu/pool/main/z/zip/zip_3.0-11build1_amd64.deb 
+dpkg -i ./zip_3.0-11build1_amd64.deb
+wget http://archive.ubuntu.com/ubuntu/pool/main/u/unzip/unzip_6.0-25ubuntu1_amd64.deb
+dpkg -i ./unzip_6.0-25ubuntu1_amd64.deb
 chmod +x ./*.sh
